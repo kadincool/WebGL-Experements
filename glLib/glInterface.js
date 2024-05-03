57cf973cb58b0384dada6b24df202616710e91ba
kadin <kadinalone@gmail.com>
2024-05-03 10:28:55 -0600

    added moving objects

--- glLib/glInterface.js
+++ glLib/glInterface.js
@@ -1,27 +1,61 @@
-var renderModel = {
-  v: new Array(),
-  i: new Array(),
-  c: new Array()
+function newModel() {
+  return {
+    v: new Array(),
+    i: new Array(),
+    c: new Array()
+  };
+}
+
+var renderModel = newModel();
+
+function clearModel(toModel = renderModel) {
+  if (!toModel.v || !toModel.i || !toModel.c) {
+    console.error("Model lacks writable data");
+    return;
+  }
+
+  toModel.v = new Array();
+  toModel.i = new Array();
+  toModel.c = new Array();
 };
 
-function loadModel(model, transform = new DOMMatrix(), color) {
+function setModel(model, toModel = renderModel) {
   if (!model.v || !model.i || !model.c) {
     console.error("Model lacks necessary information");
     return;
   }
-  let modelBegin = renderModel.v.length / 3;
+  if (!toModel.v || !toModel.i || !toModel.c) {
+    console.error("Model lacks writable data");
+    return;
+  }
+
+  toModel.v = Array.from(model.v);
+  toModel.i = Array.from(model.i);
+  toModel.c = Array.from(model.c);
+}
+
+function loadModel(model, transform = new DOMMatrix(), color, toModel = renderModel) {
+  if (!model.v || !model.i || !model.c) {
+    console.error("Model lacks necessary information");
+    return;
+  }
+  if (!toModel.v || !toModel.i || !toModel.c) {
+    console.error("Model lacks writable data");
+    return;
+  }
+  let modelBegin = toModel.v.length / 3;
   for (let i = 0; i < model.v.length; i+=3) {
     let transPos = multiplyByMatrix([model.v[i], model.v[i+1], model.v[i+2], 1], transform);
-    renderModel.v.push(transPos[0], transPos[1], transPos[2]);
+    toModel.v.push(transPos[0], transPos[1], transPos[2]);
   }
   for (let i = 0; i < model.i.length; i++) {
-    renderModel.i.push(model.i[i] + modelBegin);
+    toModel.i.push(model.i[i] + modelBegin);
   }
   for (let i = 0; i < model.c.length; i++) {
     if (color) {
-      renderModel.c.push(color[i%3]);
+      toModel.c.push(color[i%3]);
     } else {
-      renderModel.c.push(model.c[i]);
+      toModel.c.push(model.c[i]);
     }
   }
 };
@@ -58,20 +92,22 @@ gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.enable(gl.CULL_FACE);
 gl.enable(gl.DEPTH_TEST);
 
-function render(perspectiveMatrix, cameraMatrix) {
+function render(perspectiveMatrix, cameraMatrix, data = [renderModel]) {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  bufferData(program, "vertex", new Float32Array(renderModel.v), 3, gl.FLOAT);
-  bufferData(program, "color", new Float32Array(renderModel.c), 3, gl.FLOAT);
-  let indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(renderModel.i), gl.STATIC_DRAW);
-  //gl.deleteBuffer(indexBuffer);
+  for (let i in data) {
+    bufferData(program, "vertex", new Float32Array(data[i].v), 3, gl.FLOAT);
+    bufferData(program, "color", new Float32Array(data[i].c), 3, gl.FLOAT);
+    let indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data[i].i), gl.STATIC_DRAW);
+    //gl.deleteBuffer(indexBuffer);
 
-  let perspectiveUniform = gl.getUniformLocation(program, "perspective");
-  gl.uniformMatrix4fv(perspectiveUniform, false, perspectiveMatrix.toFloat32Array());
+    let perspectiveUniform = gl.getUniformLocation(program, "perspective");
+    gl.uniformMatrix4fv(perspectiveUniform, false, perspectiveMatrix.toFloat32Array());
 
-  let cameraUniform = gl.getUniformLocation(program, "camera");
-  gl.uniformMatrix4fv(cameraUniform, false, cameraMatrix.toFloat32Array());
+    let cameraUniform = gl.getUniformLocation(program, "camera");
+    gl.uniformMatrix4fv(cameraUniform, false, cameraMatrix.toFloat32Array());
 
-  gl.drawElements(gl.TRIANGLES, renderModel.i.length, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, data[i].i.length, gl.UNSIGNED_SHORT, 0);
+  }
 }
